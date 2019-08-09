aa7a376cb096a8d11e350c194d37a93a20910a2a
Cory Hedeen <hedeencory@gmail.com>
2019-08-09 09:37:00 -0400

    Refactor and move state to App

--- at-bat/src/components/Dashboard.js
+++ at-bat/src/components/Dashboard.js
@@ -2,43 +2,15 @@ import React, { useState } from 'react';
 import Display from './Display';
 
 const DashBoard = (props) => {
-  const [ball, setBall] = useState(0);
-  const [strike, setStrike] = useState(0);
 
-function foul(){
- if(strike < 2){
-   setStrike(strike + 1)
- }
-}
-
-function hit(){
-  setBall(0);
-  setStrike(0);
-}
-
-function ballCount(){
-  if(ball < 4){
-    setBall(ball + 1)
-  } else if(ball === 4){
-    setBall(0)
-  }
-}
-
-function strikeCount(){
-  if(strike < 3){
-    setStrike(strike + 1)
-  } else if(strike === 3){
-    setStrike(0);
-  }
-}
 
   return (
     <div className="dashBoard">
-      <Display balls={ball} strikes={strike}/>
-        <button onClick={() => strikeCount()}>strike</button>
-        <button onClick={() => ballCount()}>ball</button>
-        <button onClick={() => foul()}>foul</button>
-        <button onClick={() => hit()}>hit</button>
+      <Display balls={props.ball} strikes={props.strike}/>
+        <button onClick={props.strikeCount}>strike</button>
+        <button onClick={props.ballCount}>ball</button>
+        <button onClick={props.foul}>foul</button>
+        <button onClick={props.hit}>hit</button>
 
     </div>
   );
